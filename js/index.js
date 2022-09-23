f6bee63d23c36ddeb53c8dcf836b180652a4f993
rashiddalv <rashiddalv@gmail.com>
2022-09-23 23:32:07 +0400

    Choose Your Fighter (Sound fix)

--- js/index.js
+++ js/index.js
@@ -127,4 +127,23 @@ function sound(){
 
 
 
+// function none(e){
+//     e.style.background = "purple"
+//     e.classList.add("div_border")
+// }
 
+
+
+
+// function divland() {
+//     let div_count = parseInt(document.getElementById("div_count").value);
+//     let person = ['raihen','luikang']
+//     for (x = 1; x <= div_count; x++) {
+//       var boardik = document.createElement("div");
+//       boardik.innerHTML = x;
+//       boardik.className = "blah";
+//       boardik.classList.add("a"+x)
+//       boardik.setAttribute("onclick", "none(this)");
+//       document.getElementById("board").appendChild(boardik);
+//     }
+//   }
